160856cdba44e28fb0bcbeb53a83375b71ac7b24
Austin Brenner <austin.m.brenner@nasa.gov>
2026-01-06 11:43:38 -0500

    updates to sat data collection

--- scripts/scrape_spacecraft.py
+++ scripts/scrape_spacecraft.py
@@ -1,9 +1,9 @@
 #/usr/bin/env python
-"""accesses WIND data from nasa CDA and creates IMF_new.dat for SWMF input
+"""accesses satellite data from nasa CDA and creates <sat>.txt for SWMF input
 """
 import os,sys
-sys.path.append(os.getcwd().split('swmf-energetics')[0]+
-                                      'swmf-energetics/')
+sys.path.append(os.getcwd().split('magnetosphere-energetics')[0]+
+                                      'magnetosphere-energetics/')
 import datetime as dt
 import numpy as np
 from matplotlib import pyplot as plt
@@ -39,21 +39,14 @@ def write_SWMF_satfile(df,outname,outpath,**kwargs):
             line.append(f'{y:.2f}')
             line.append(f'{z:.2f}')
             f.write(''.join([s.rjust(7) for s in line])[3::]+'\n')
-        print(f'\tCreated {os.path.join(outpath,outname)}')
+        print(f'\t\003[92m Created\003[00m {os.path.join(outpath,outname)}')
 
 #Main program
-if __name__ == '__main__':
-    #############################USER INPUTS HERE##########################
-    start = dt.datetime(2024,5,10,6)
-    end = dt.datetime(2024,5,13,0)
-    outpath = './gannon_storm/'
-    plot_data = False
-    #######################################################################
-
+def main() -> None:
     # Scrape data from CDAWeb
     goes_pos, goes_b, goes_plasma = collect_goes(start,end,
                                             skip_bfield=True,skip_plasma=True,
-                                            probes=['16'],
+                                            probes=['16','17'],
                                             writeData=True)
     cluster_pos, cluster_b,cluster_plasma = collect_cluster(start, end,
                                             skip_bfield=True,skip_plasma=True,
@@ -61,46 +54,51 @@ if __name__ == '__main__':
                                             writeData=True)
     mms_pos, mms_b,mms_plasma = collect_mms(start, end,
                                             skip_bfield=True,skip_plasma=True,
-                                            probes=['1','2','3','4'],
+                                            probes=['1'],
                                             writeData=True)
     themis_pos, themis_b,themis_plasma = collect_themis(start, end,
                                             skip_bfield=True,skip_plasma=True,
-                                            probes=['A','B','C','D','E'],
+                                            probes=['A','D','E'],
                                             writeData=True)
 
     # Quick Plot
-    quicklook,(equitorial,meridional) =plt.subplots(1,2,figsize=[20,10])
-    for sat in cluster_pos.keys():
-        equitorial.scatter(cluster_pos[sat]['x_gsm'],cluster_pos[sat]['y_gsm'],
-                    label=sat)
-        meridional.scatter(cluster_pos[sat]['x_gsm'],cluster_pos[sat]['z_gsm'],
-                    label=sat)
-    for sat in themis_pos.keys():
-        equitorial.scatter(themis_pos[sat]['x_gsm'],themis_pos[sat]['y_gsm'],
-                    label=sat)
-        meridional.scatter(themis_pos[sat]['x_gsm'],themis_pos[sat]['z_gsm'],
-                    label=sat)
-    for sat in mms_pos.keys():
-        equitorial.scatter(mms_pos[sat]['x_gsm'],mms_pos[sat]['y_gsm'],
-                           label=sat)
-        meridional.scatter(mms_pos[sat]['x_gsm'],mms_pos[sat]['z_gsm'],
-                           label=sat)
-    for sat in goes_pos.keys():
-        equitorial.scatter(goes_pos[sat]['x_gsm'],goes_pos[sat]['y_gsm'],
-                           label=sat)
-        meridional.scatter(goes_pos[sat]['x_gsm'],goes_pos[sat]['z_gsm'],
-                           label=sat)
-    equitorial.set_xlim(20,-20)
-    equitorial.set_ylim(20,-20)
-    equitorial.set_xlabel('X [R]')
-    equitorial.set_ylabel('Y [R]')
-    equitorial.legend()
-    meridional.set_xlim(20,-20)
-    meridional.set_ylim(-20,20)
-    meridional.set_xlabel('X [R]')
-    meridional.set_ylabel('Z [R]')
-    meridional.legend()
-    from IPython import embed; embed()
+    if PLOT_DATA:
+        quicklook,(equitorial,meridional) =plt.subplots(1,2,figsize=[20,10])
+        for sat in cluster_pos.keys():
+            equitorial.scatter(cluster_pos[sat]['x_gsm'],
+                               cluster_pos[sat]['y_gsm'],
+                        label=sat)
+            meridional.scatter(cluster_pos[sat]['x_gsm'],
+                               cluster_pos[sat]['z_gsm'],
+                        label=sat)
+        for sat in themis_pos.keys():
+            equitorial.scatter(themis_pos[sat]['x_gsm'],
+                               themis_pos[sat]['y_gsm'],
+                        label=sat)
+            meridional.scatter(themis_pos[sat]['x_gsm'],
+                               themis_pos[sat]['z_gsm'],
+                        label=sat)
+        for sat in mms_pos.keys():
+            equitorial.scatter(mms_pos[sat]['x_gsm'],mms_pos[sat]['y_gsm'],
+                            label=sat)
+            meridional.scatter(mms_pos[sat]['x_gsm'],mms_pos[sat]['z_gsm'],
+                            label=sat)
+        for sat in goes_pos.keys():
+            equitorial.scatter(goes_pos[sat]['x_gsm'],goes_pos[sat]['y_gsm'],
+                            label=sat)
+            meridional.scatter(goes_pos[sat]['x_gsm'],goes_pos[sat]['z_gsm'],
+                            label=sat)
+        equitorial.set_xlim(20,-20)
+        equitorial.set_ylim(20,-20)
+        equitorial.set_xlabel('X [R]')
+        equitorial.set_ylabel('Y [R]')
+        equitorial.legend()
+        meridional.set_xlim(20,-20)
+        meridional.set_ylim(-20,20)
+        meridional.set_xlabel('X [R]')
+        meridional.set_ylabel('Z [R]')
+        meridional.legend()
+        from IPython import embed; embed()
 
     print('Writing Cluster Satfiles ...')
     for sat in cluster_pos.keys():
@@ -118,3 +116,14 @@ if __name__ == '__main__':
     for sat in goes_pos.keys():
         write_SWMF_satfile(goes_pos[sat],sat+'.txt','./gannon_storm',
                            note=f'{sat} Created {str(dt.datetime.now())}')
+
+if __name__ == '__main__':
+    global START,END,OUTPATH,PLOT_DATA
+    #############################USER INPUTS HERE##########################
+    START     = dt.datetime(2019,5,13,12,0)
+    END       = dt.datetime(2019,5,15,12,0)
+    OUTPATH   = './energy_map/inputs/satellites'
+    PLOT_DATA = True
+    #######################################################################
+
+    main()
